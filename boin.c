a69aec62f608189d4de6cbc7f74300786528efe0
EnoS4 <saya.loveup.99@gmail.com>
2016-04-14 23:29:43 +0900

    Create boin.c

---
+++ boin.c
@@ -0,0 +1,17 @@
+/*入力された５つの数字の中の最大値を出力するプログラムを作成せよ*/
+#include<stdio.h>
+#include<string.h>
+
+int main(void) {
+  int cnt, boin;
+  char /*入力された５つの数字の中の最大値を出力するプログラムを作成せよ*/
+
+  for (cnt = 0; cnt < 5; cnt++) {
+    printf("数字を入力してください\n", cnt + 1);
+    scanf("%d %d %d %d %d", &number[0], &number[2], &number[3], &number[4], &number[5]);// 
+  }
+
+
+  return 0;
+  
+}
