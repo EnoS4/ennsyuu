050ba701f9206319641732913f84665c126932a7
EnoS4 <saya.loveup.99@gmail.com>
2016-04-21 00:22:39 +0900

    Update pointa1.c
    配列使用の環状リスト

--- pointa1.c
+++ pointa1.c
@@ -1,1 +1,34 @@
-#include<>
+
+#include<stdio.h>
+#include<string.h>
+
+#define max 5
+
+typedef struct EMPLOYEE {
+	char name[16];
+} EMPLOYEE;
+
+int main(void)
+{
+	EMPLOYEE empArray[3];
+	int i,j;
+	int arraySize;
+
+	strcpy(empArray[0].name, "池田 利栄");
+	strcpy(empArray[1].name, "田中 賢吾");
+	strcpy(empArray[2].name, "中野 秀樹");
+	arraySize = 3;
+
+	for (j = 0; j < max; j++) {
+		for (i = 0; i < arraySize; i++) {
+			printf("%sさん \n", empArray[i].name);
+		}
+		if ( i > arraySize ) {
+			i = 0;
+		}
+		for (i = 0; i < arraySize; i++) {
+			printf("%sさん \n", empArray[i].name);
+		}
+	}
+	return 0;
+}
