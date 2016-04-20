40257d4b7e097e98a4aefa3a5e4ba0bc78afd9b7
EnoS4 <saya.loveup.99@gmail.com>
2016-04-20 22:17:35 +0900

    Create boin.c

---
+++ boin.c
@@ -0,0 +1,33 @@
+/*入力された英字の中から母音を出力するプログラムを作成せよ*/
+#include<stdio.h>
+#include<string.h>
+
+#define max 256
+
+int main(void) {
+  int cnt;
+  char imp[max];
+  printf("英字を入力してください\n", imp[cnt]);
+    scanf("%s\n", imp[cnt]);//
+  for (cnt = 0; cnt < strlen(imp); cnt++) {
+    switch (imp[cnt]){
+      case'a':
+      printf("%s\n",imp[cnt]);
+      break;
+      case'i':
+      printf("%s\n",imp[cnt]);
+      break;
+      case'u':
+      printf("%s\n",imp[cnt]);
+      break;
+      case'e':
+      printf("%s\n",imp[cnt]);
+      break;
+      case'o':
+      printf("%s\n",imp[cnt]);
+      break;
+    }
+    printf("\n");
+  }
+  return 0;
+}
